f20cd3c397b0cfa37835f841848694c6b58eb9aa
Govindaraj-B <govi1012000@gmail.com>
2022-10-20 11:58:09 +0530

    added

--- config/database.js
+++ config/database.js
@@ -11,23 +11,23 @@
 //     },
 //   },
 // });
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'database-1.cj1omoyequnu.ap-south-1.rds.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'initial_db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'govindarajdeveloper180262333355'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
-    },
-    debug: false,
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'database-1.cj1omoyequnu.ap-south-1.rds.amazonaws.com'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'initial_db'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'govindarajdeveloper180262333355'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
 
 
 // module.exports = ({ env }) => ({
@@ -47,3 +47,22 @@ module.exports = ({ env }) => ({
 //     debug: false,
 //   },
 // });
+
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'database-1.cfanbku6eg5n.ap-south-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'initial_db'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'govindarajdeveloper180262333355'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
